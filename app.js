bd59199b758dd1a95e1e0bf2ae9beec16e84ae34
Christian Alan Tambaoan <catambaoan@gmail.com>
2021-12-19 22:09:03 +0800

    Rework

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const button = document.querySelector('#color-btn');
-const randomColor = () => Math.floor(Math.random() * 255) + 1;
+
+function randomColor() {
+  const clr1 = Math.floor(Math.random() * 255) + 1;
+  const clr2 = Math.floor(Math.random() * 255) + 1;
+  const clr3 = Math.floor(Math.random() * 255) + 1;
+
+  return `rgb(${clr1}, ${clr2}, ${clr3})`;
+}
 
 function changeBgColor() {
   const colorText = document.querySelector('#color');
-  const clr1 = randomColor();
-  const clr2 = randomColor();
-  const clr3 = randomColor();
+  const colorA = randomColor();
+  const colorB = randomColor();
 
-  /* document.body.style.backgroundColor = `rgb(${clr1}, ${clr2}, ${clr3})`; */
-  document.body.style.background = `linear-gradient(
-    to right, 
-    rgb(${clr1}, ${clr2}, ${clr3}), 
-    rgb(${clr3}, ${clr1}, ${clr2})
-    )`;
-
-  colorText.textContent = `rgb(${clr1}, ${clr2}, ${clr3}) +
-    rgb(${clr3}, ${clr1}, ${clr2})`;
+  document.body.style.background = `linear-gradient(to right, ${colorA}, ${colorB})`;
+  colorText.textContent = `${colorA} -> ${colorB}`;
 }
 
 button.addEventListener('click', changeBgColor);

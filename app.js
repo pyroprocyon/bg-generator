1fa3c1770861a61489413bfc4712054b02f3463f
Christian Alan Tambaoan <pyroprocyon02@gmail.com>
2022-05-03 10:02:38 +0800

    Refactor

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const button = document.querySelector('#color-btn');
 
-function randomColor() {
-  const clr1 = Math.floor(Math.random() * 255) + 1;
-  const clr2 = Math.floor(Math.random() * 255) + 1;
-  const clr3 = Math.floor(Math.random() * 255) + 1;
+function random() {
+   return Math.floor(Math.random() * 255) + 1;
+}
 
-  return `rgb(${clr1}, ${clr2}, ${clr3})`;
+function randomColor() {
+   return `rgb(${random()}, ${random()}, ${random()})`;
 }
 
 function changeBgColor() {
-  const colorText = document.querySelector('#color');
-  const colorA = randomColor();
-  const colorB = randomColor();
+   const colorText = document.querySelector('#color');
+   const colorA = randomColor();
+   const colorB = randomColor();
 
-  document.body.style.background = `linear-gradient(to right, ${colorA}, ${colorB})`;
-  colorText.textContent = `${colorA} -> ${colorB}`;
+   document.body.style.background = `linear-gradient(to right, ${colorA}, ${colorB})`;
+   colorText.textContent = `${colorA} -> ${colorB}`;
 }
 
 button.addEventListener('click', changeBgColor);
